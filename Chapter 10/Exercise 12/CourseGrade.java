b69123ede263235ee495a5702a062f57556445b1
sojaps <japs.so@norimaconsulting.com>
2022-04-20 16:36:14 +0800

    chapter 10

---
+++ Chapter 10/Exercise 12/CourseGrade.java
@@ -0,0 +1,8 @@
+public class CourseGrade {
+    public static void main(String[] args) {
+        GradeBook gradeBook = new GradeBook();
+        gradeBook.getGrades();
+        gradeBook.studentAvg(12);
+        gradeBook.testAvg(3);
+    }
+}
---
+++ Chapter 10/Exercise 12/GradeBook.java
@@ -0,0 +1,50 @@
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GradeBook {
+    private int[][] grades = new int[12][5];
+    private Scanner input = new Scanner(System.in);
+
+    public void getGrades(){
+        int id =0;
+        do {
+            System.out.printf("\nENTERING SCORES FOR STUDENT ID %d\n\n",id+1);
+            for (int i = 0; i <5; i++) {
+                System.out.printf("Enter score for exam %d: ",i+1);
+                grades[id][i]=Integer.parseInt(input.nextLine());
+            }
+            id++;
+        } while (id<grades.length);
+    }
+
+    public void showGrades(){
+        int counter=1;
+        int counter2=1;
+        for (int[] is : grades) {
+            System.out.printf("==============Student %d==============\n\n",counter);
+            for (int i : is) {
+                System.out.printf("Score %d: %d",counter2,i );
+                counter2++;
+            }
+            counter++;
+            counter2=1;
+        }
+    }
+
+    public void studentAvg(int studentNumber){
+        int sum=0;
+        for (int is : grades[studentNumber-1]) {
+            sum +=is;
+        }
+        System.out.printf("Student %d average: %d",studentNumber,sum/5);
+    }
+
+    public void testAvg(int testNumber){
+        int sum=0;
+        for (int[] is : grades) {
+            sum += is[testNumber];
+        }
+        System.out.printf("Test %d average: %d",testNumber,sum/12);
+    }
+}
